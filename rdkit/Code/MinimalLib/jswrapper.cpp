b64bd699a013d379550c40434e49cc69c0d59da1
ali1810 <49233294+ali1810@users.noreply.github.com>
2021-09-11 18:20:24 +0200

    Add files via upload

---
+++ rdkit/Code/MinimalLib/jswrapper.cpp
@@ -0,0 +1,208 @@
+//
+//
+//  Copyright (C) 2019-2021 Greg Landrum and other RDKit contributors
+//
+//   @@ All Rights Reserved @@
+//  This file is part of the RDKit.
+//  The contents are covered by the terms of the BSD license
+//  which is included in the file license.txt, found at the root
+//  of the RDKit source tree.
+//
+#include <emscripten.h>
+#include <emscripten/val.h>
+#include <emscripten/bind.h>
+#include "minilib.h"
+#include <GraphMol/MolDraw2D/MolDraw2D.h>
+#include <GraphMol/MolDraw2D/MolDraw2DUtils.h>
+#include <GraphMol/MolDraw2D/MolDraw2DJS.h>
+
+using namespace RDKit;
+
+namespace RDKit {
+namespace MinimalLib {
+extern std::string process_details(const std::string &details,
+                                   unsigned int &width, unsigned int &height,
+                                   int &offsetx, int &offsety,
+                                   std::string &legend,
+                                   std::vector<int> &atomIds,
+                                   std::vector<int> &bondIds);
+}
+}  // namespace RDKit
+
+namespace {
+std::string draw_to_canvas_with_offset(JSMol &self, emscripten::val canvas,
+                                       int offsetx, int offsety, int width,
+                                       int height) {
+  if (!self.d_mol) {
+    return "no molecule";
+  }
+  auto ctx = canvas.call<emscripten::val>("getContext", std::string("2d"));
+  if (width < 0) {
+    width = canvas["width"].as<int>();
+  }
+  if (height < 0) {
+    height = canvas["height"].as<int>();
+  }
+  MolDraw2DJS *d2d = new MolDraw2DJS(width, height, ctx);
+  d2d->setOffset(offsetx, offsety);
+  MolDraw2DUtils::prepareAndDrawMolecule(*d2d, *self.d_mol);
+  delete d2d;
+  return "";
+}
+
+std::string draw_to_canvas(JSMol &self, emscripten::val canvas, int width,
+                           int height) {
+  return draw_to_canvas_with_offset(self, canvas, 0, 0, width, height);
+}
+
+std::string draw_to_canvas_with_highlights(JSMol &self, emscripten::val canvas,
+                                           const std::string &details) {
+  if (!self.d_mol) return "";
+
+  std::vector<int> atomIds;
+  std::vector<int> bondIds;
+
+  auto ctx = canvas.call<emscripten::val>("getContext", std::string("2d"));
+
+  unsigned int w = canvas["width"].as<unsigned int>();
+  unsigned int h = canvas["height"].as<unsigned int>();
+  int offsetx = 0;
+  int offsety = 0;
+  std::string legend = "";
+  auto problems = MinimalLib::process_details(details, w, h, offsetx, offsety,
+                                              legend, atomIds, bondIds);
+  if (!problems.empty()) {
+    return problems;
+  }
+
+  MolDraw2DJS *d2d = new MolDraw2DJS(w, h, ctx);
+  if (!details.empty()) {
+    MolDraw2DUtils::updateDrawerParamsFromJSON(*d2d, details);
+  }
+  d2d->setOffset(offsetx, offsety);
+
+  MolDraw2DUtils::prepareAndDrawMolecule(*d2d, *self.d_mol, legend, &atomIds,
+                                         &bondIds);
+  delete d2d;
+  return "";
+}
+
+JSMol *get_mol_no_details(const std::string &input) {
+  return get_mol(input, std::string());
+}
+
+emscripten::val get_morgan_fp_as_uint8array(const JSMol &self,
+                                            unsigned int radius,
+                                            unsigned int fplen) {
+  std::string fp = self.get_morgan_fp_as_binary_text(radius, fplen);
+  emscripten::val view(emscripten::typed_memory_view(
+      fp.size(), reinterpret_cast<const unsigned char *>(fp.c_str())));
+  auto res = emscripten::val::global("Uint8Array").new_(fp.size());
+  res.call<void>("set", view);
+  return res;
+}
+
+emscripten::val get_morgan_fp_as_uint8array(const JSMol &self) {
+  return get_morgan_fp_as_uint8array(self, 2, 2048);
+}
+
+}  // namespace
+
+using namespace emscripten;
+EMSCRIPTEN_BINDINGS(RDKit_minimal) {
+  class_<JSMol>("Mol")
+      .function("is_valid", &JSMol::is_valid)
+      .function("get_smiles", &JSMol::get_smiles)
+      .function("get_cxsmiles", &JSMol::get_cxsmiles)
+      .function("get_molblock", &JSMol::get_molblock)
+      .function("get_v3Kmolblock", &JSMol::get_v3Kmolblock)
+      .function("get_inchi", &JSMol::get_inchi)
+      .function("get_json", &JSMol::get_json)
+      .function("get_svg",
+                select_overload<std::string() const>(&JSMol::get_svg))
+      .function("get_svg",
+                select_overload<std::string(unsigned int, unsigned int) const>(
+                    &JSMol::get_svg))
+
+      .function("get_svg_with_highlights", &JSMol::get_svg_with_highlights)
+#ifdef __EMSCRIPTEN__
+      .function("draw_to_canvas_with_offset", &draw_to_canvas_with_offset)
+      .function("draw_to_canvas", &draw_to_canvas)
+      .function("draw_to_canvas_with_highlights",
+                &draw_to_canvas_with_highlights)
+      .function("get_morgan_fp_as_uint8array",
+                select_overload<emscripten::val(const JSMol &)>(
+                    get_morgan_fp_as_uint8array))
+      .function("get_morgan_fp_as_uint8array",
+                select_overload<emscripten::val(const JSMol &, unsigned int,
+                                                unsigned int)>(
+                    get_morgan_fp_as_uint8array))
+#endif
+      .function("get_substruct_match", &JSMol::get_substruct_match)
+      .function("get_substruct_matches", &JSMol::get_substruct_matches)
+      .function("get_descriptors", &JSMol::get_descriptors)
+      .function("get_morgan_fp",
+                select_overload<std::string() const>(&JSMol::get_morgan_fp))
+      .function("get_morgan_fp",
+                select_overload<std::string(unsigned int, unsigned int) const>(
+                    &JSMol::get_morgan_fp))
+
+      // functionality primarily useful in ketcher
+      .function("get_stereo_tags", &JSMol::get_stereo_tags)
+      .function("get_aromatic_form", &JSMol::get_aromatic_form)
+      .function("get_kekule_form", &JSMol::get_kekule_form)
+      .function("get_new_coords",
+                select_overload<std::string() const>(&JSMol::get_new_coords))
+      .function("get_new_coords", select_overload<std::string(bool) const>(
+                                      &JSMol::get_new_coords))
+      .function("generate_aligned_coords",
+                select_overload<std::string(const JSMol &)>(
+                    &JSMol::generate_aligned_coords))
+      .function("generate_aligned_coords",
+                select_overload<std::string(const JSMol &, bool)>(
+                    &JSMol::generate_aligned_coords))
+      .function("generate_aligned_coords",
+                select_overload<std::string(const JSMol &, bool, bool)>(
+                    &JSMol::generate_aligned_coords))
+      .function("generate_aligned_coords",
+                select_overload<std::string(const JSMol &, bool, bool, bool)>(
+                    &JSMol::generate_aligned_coords))
+      .function("condense_abbreviations",
+                select_overload<std::string()>(&JSMol::condense_abbreviations))
+      .function("condense_abbreviations",
+                select_overload<std::string(double, bool)>(
+                    &JSMol::condense_abbreviations))
+      .function("add_hs", &JSMol::add_hs)
+      .function("remove_hs", &JSMol::remove_hs);
+
+  class_<JSSubstructLibrary>("SubstructLibrary")
+      .constructor<>()
+      .constructor<unsigned int>()
+      .function("add_mol", &JSSubstructLibrary::add_mol)
+      .function("add_smiles", &JSSubstructLibrary::add_smiles)
+      .function("add_trusted_smiles", &JSSubstructLibrary::add_trusted_smiles)
+      .function("get_mol", &JSSubstructLibrary::get_mol, allow_raw_pointers())
+      .function(
+          "get_matches",
+          select_overload<std::string(const JSMol &, bool, int, int) const>(
+              &JSSubstructLibrary::get_matches))
+      .function("get_matches",
+                select_overload<std::string(const JSMol &, int) const>(
+                    &JSSubstructLibrary::get_matches))
+      .function("get_matches",
+                select_overload<std::string(const JSMol &) const>(
+                    &JSSubstructLibrary::get_matches))
+      .function("count_matches",
+                select_overload<unsigned int(const JSMol &, bool, int) const>(
+                    &JSSubstructLibrary::count_matches))
+      .function("count_matches",
+                select_overload<unsigned int(const JSMol &) const>(
+                    &JSSubstructLibrary::count_matches));
+
+  function("version", &version);
+  function("prefer_coordgen", &prefer_coordgen);
+  function("get_inchikey_for_inchi", &get_inchikey_for_inchi);
+  function("get_mol", &get_mol, allow_raw_pointers());
+  function("get_mol", &get_mol_no_details, allow_raw_pointers());
+  function("get_qmol", &get_qmol, allow_raw_pointers());
+}
---
+++ rdkit/Code/MinimalLib/minilib.cpp
@@ -0,0 +1,429 @@
+//
+//
+//  Copyright (C) 2019-2021 Greg Landrum and other RDKit contributors
+//
+//   @@ All Rights Reserved @@
+//  This file is part of the RDKit.
+//  The contents are covered by the terms of the BSD license
+//  which is included in the file license.txt, found at the root
+//  of the RDKit source tree.
+//
+#include <string>
+#include <iostream>
+#include "minilib.h"
+#include "common.h"
+
+#include <RDGeneral/versions.h>
+#include <GraphMol/RDKitBase.h>
+#include <GraphMol/MolPickler.h>
+#include <GraphMol/SmilesParse/SmilesParse.h>
+#include <GraphMol/FileParsers/FileParsers.h>
+#include <GraphMol/MolDraw2D/MolDraw2D.h>
+#include <GraphMol/MolDraw2D/MolDraw2DSVG.h>
+#include <GraphMol/MolDraw2D/MolDraw2DUtils.h>
+#include <GraphMol/Substruct/SubstructMatch.h>
+#include <GraphMol/SubstructLibrary/SubstructLibrary.h>
+#include <GraphMol/Descriptors/Property.h>
+#include <GraphMol/Descriptors/MolDescriptors.h>
+#include <GraphMol/Fingerprints/MorganFingerprints.h>
+#include <GraphMol/MolInterchange/MolInterchange.h>
+#include <GraphMol/Depictor/RDDepictor.h>
+#include <GraphMol/CIPLabeler/CIPLabeler.h>
+#include <GraphMol/Abbreviations/Abbreviations.h>
+#include <DataStructs/BitOps.h>
+
+#include <INCHI-API/inchi.h>
+
+#include <rapidjson/document.h>
+#include <rapidjson/stringbuffer.h>
+#include <rapidjson/writer.h>
+
+namespace rj = rapidjson;
+
+using namespace RDKit;
+
+std::string JSMol::get_smiles() const {
+  if (!d_mol) return "";
+  return MolToSmiles(*d_mol);
+}
+std::string JSMol::get_cxsmiles() const {
+  if (!d_mol) return "";
+  return MolToCXSmiles(*d_mol);
+}
+std::string JSMol::get_svg(unsigned int w, unsigned int h) const {
+  if (!d_mol) return "";
+  return MinimalLib::mol_to_svg(*d_mol, w, h);
+}
+std::string JSMol::get_svg_with_highlights(const std::string &details) const {
+  if (!d_mol) return "";
+
+  unsigned int w = MinimalLib::d_defaultWidth;
+  unsigned int h = MinimalLib::d_defaultHeight;
+  return MinimalLib::mol_to_svg(*d_mol, w, h, details);
+}
+
+std::string JSMol::get_inchi() const {
+  if (!d_mol) return "";
+  ExtraInchiReturnValues rv;
+  return MolToInchi(*d_mol, rv);
+}
+std::string JSMol::get_molblock() const {
+  if (!d_mol) return "";
+  return MolToMolBlock(*d_mol);
+}
+std::string JSMol::get_v3Kmolblock() const {
+  if (!d_mol) return "";
+  return MolToV3KMolBlock(*d_mol);
+}
+std::string JSMol::get_json() const {
+  if (!d_mol) return "";
+  return MolInterchange::MolToJSONData(*d_mol);
+}
+
+std::string JSMol::get_substruct_match(const JSMol &q) const {
+  std::string res = "{}";
+  if (!d_mol || !q.d_mol) return res;
+
+  MatchVectType match;
+  if (SubstructMatch(*d_mol, *(q.d_mol), match)) {
+    rj::Document doc;
+    doc.SetObject();
+    MinimalLib::get_sss_json(*d_mol, *(q.d_mol), match, doc, doc);
+    rj::StringBuffer buffer;
+    rj::Writer<rj::StringBuffer> writer(buffer);
+    doc.Accept(writer);
+    res = buffer.GetString();
+  }
+
+  return res;
+}
+
+std::string JSMol::get_substruct_matches(const JSMol &q) const {
+  std::string res = "{}";
+  if (!d_mol || !q.d_mol) return res;
+
+  auto matches = SubstructMatch(*d_mol, (*q.d_mol));
+  if (!matches.empty()) {
+    rj::Document doc;
+    doc.SetArray();
+
+    for (const auto &match : matches) {
+      rj::Value rjMatch(rj::kObjectType);
+      MinimalLib::get_sss_json(*d_mol, *(q.d_mol), match, rjMatch, doc);
+      doc.PushBack(rjMatch, doc.GetAllocator());
+    }
+
+    rj::StringBuffer buffer;
+    rj::Writer<rj::StringBuffer> writer(buffer);
+    doc.Accept(writer);
+    res = buffer.GetString();
+  }
+
+  return res;
+}
+
+std::string JSMol::get_descriptors() const {
+  if (!d_mol) return "{}";
+  return MinimalLib::get_descriptors(*d_mol);
+}
+
+std::string JSMol::get_morgan_fp(unsigned int radius,
+                                 unsigned int fplen) const {
+  if (!d_mol) return "";
+  auto fp = MorganFingerprints::getFingerprintAsBitVect(*d_mol, radius, fplen);
+  std::string res = BitVectToText(*fp);
+  delete fp;
+  return res;
+}
+
+std::string JSMol::get_morgan_fp_as_binary_text(unsigned int radius,
+                                                unsigned int fplen) const {
+  if (!d_mol) return "";
+  auto fp = MorganFingerprints::getFingerprintAsBitVect(*d_mol, radius, fplen);
+  std::string res = BitVectToBinaryText(*fp);
+  delete fp;
+  return res;
+}
+
+std::string JSMol::get_stereo_tags() const {
+  if (!d_mol) return "{}";
+  rj::Document doc;
+  doc.SetObject();
+
+  bool cleanIt = true;
+  bool force = true;
+  bool flagPossibleStereocenters = true;
+  MolOps::assignStereochemistry(*d_mol, cleanIt, force,
+                                flagPossibleStereocenters);
+  CIPLabeler::assignCIPLabels(*d_mol);
+
+  rj::Value rjAtoms(rj::kArrayType);
+  for (const auto atom : d_mol->atoms()) {
+    std::string cip;
+    if (!atom->getPropIfPresent(common_properties::_CIPCode, cip)) {
+      if (atom->hasProp(common_properties::_ChiralityPossible)) {
+        cip = "?";
+      }
+    }
+    if (!cip.empty()) {
+      cip = "(" + cip + ")";
+      rj::Value entry(rj::kArrayType);
+      entry.PushBack(atom->getIdx(), doc.GetAllocator());
+      rj::Value v;
+      v.SetString(cip.c_str(), cip.size(), doc.GetAllocator());
+      entry.PushBack(v, doc.GetAllocator());
+      rjAtoms.PushBack(entry, doc.GetAllocator());
+    }
+  }
+  doc.AddMember("CIP_atoms", rjAtoms, doc.GetAllocator());
+
+  rj::Value rjBonds(rj::kArrayType);
+  for (const auto bond : d_mol->bonds()) {
+    std::string cip;
+    if (bond->getPropIfPresent(common_properties::_CIPCode, cip)) {
+      cip = "(" + cip + ")";
+      rj::Value entry(rj::kArrayType);
+      entry.PushBack(bond->getBeginAtomIdx(), doc.GetAllocator());
+      entry.PushBack(bond->getEndAtomIdx(), doc.GetAllocator());
+      rj::Value v;
+      v.SetString(cip.c_str(), cip.size(), doc.GetAllocator());
+      entry.PushBack(v, doc.GetAllocator());
+      rjBonds.PushBack(entry, doc.GetAllocator());
+    }
+  }
+
+  doc.AddMember("CIP_bonds", rjBonds, doc.GetAllocator());
+
+  rj::StringBuffer buffer;
+  rj::Writer<rj::StringBuffer> writer(buffer);
+  doc.Accept(writer);
+  return buffer.GetString();
+}
+
+std::string JSMol::get_aromatic_form() const {
+  if (!d_mol) return "";
+
+  RWMol molCopy(*d_mol);
+  MolOps::setAromaticity(molCopy);
+
+  bool includeStereo = true;
+  int confId = -1;
+  bool kekulize = false;
+  return MolToMolBlock(molCopy, includeStereo, confId, kekulize);
+}
+
+std::string JSMol::get_kekule_form() const {
+  if (!d_mol) return "";
+
+  RWMol molCopy(*d_mol);
+  MolOps::Kekulize(molCopy);
+
+  bool includeStereo = true;
+  int confId = -1;
+  bool kekulize = true;
+  return MolToMolBlock(molCopy, includeStereo, confId, kekulize);
+}
+
+std::string JSMol::get_new_coords(bool useCoordGen) const {
+  if (!d_mol) return "";
+
+  RWMol molCopy(*d_mol);
+
+#ifdef RDK_BUILD_COORDGEN_SUPPORT
+  bool oprefer = RDDepict::preferCoordGen;
+  RDDepict::preferCoordGen = useCoordGen;
+#endif
+  RDDepict::compute2DCoords(molCopy);
+#ifdef RDK_BUILD_COORDGEN_SUPPORT
+  RDDepict::preferCoordGen = oprefer;
+#endif
+
+  return MolToMolBlock(molCopy);
+}
+
+std::string JSMol::remove_hs() const {
+  if (!d_mol) return "";
+
+  RWMol molCopy(*d_mol);
+  MolOps::removeAllHs(molCopy);
+
+  bool includeStereo = true;
+  int confId = -1;
+  bool kekulize = true;
+  return MolToMolBlock(molCopy, includeStereo, confId, kekulize);
+}
+
+std::string JSMol::add_hs() const {
+  if (!d_mol) return "";
+
+  RWMol molCopy(*d_mol);
+  MolOps::addHs(molCopy);
+
+  // RDDepict::generateDepictionMatching2DStructure(molCopy, *d_mol);
+
+  bool includeStereo = true;
+  int confId = -1;
+  bool kekulize = true;
+  return MolToMolBlock(molCopy, includeStereo, confId, kekulize);
+}
+
+std::string JSMol::condense_abbreviations(double maxCoverage, bool useLinkers) {
+  if (!d_mol) return "";
+  if (!useLinkers) {
+    Abbreviations::condenseMolAbbreviations(
+        *d_mol, Abbreviations::Utils::getDefaultAbbreviations(), maxCoverage);
+  } else {
+    Abbreviations::condenseMolAbbreviations(
+        *d_mol, Abbreviations::Utils::getDefaultLinkers(), maxCoverage);
+  }
+  return "";
+}
+
+std::string JSMol::condense_abbreviations_from_defs(
+    const std::string &definitions, double maxCoverage, bool areLinkers) {
+  static std::string lastDefs = "";
+  static std::vector<Abbreviations::AbbreviationDefinition> abbrevs;
+  if (definitions != lastDefs) {
+    // yes, we are making the assumption that the "areLinkers" argument remains
+    // the same if the definitions are the same
+    bool removeExtraDummies = areLinkers;
+    bool allowConnectionToDummies = areLinkers;
+    lastDefs = definitions;
+    try {
+      abbrevs = Abbreviations::Utils::parseAbbreviations(
+          definitions, removeExtraDummies, allowConnectionToDummies);
+    } catch (...) {
+      return "cannot parse abbreviations";
+    }
+  }
+  Abbreviations::condenseMolAbbreviations(*d_mol, abbrevs, maxCoverage);
+}
+
+std::string JSMol::generate_aligned_coords(const JSMol &templateMol,
+                                           bool useCoordGen, bool allowRGroups,
+                                           bool acceptFailure) {
+  std::string res;
+  if (!d_mol || !templateMol.d_mol || !templateMol.d_mol->getNumConformers())
+    return res;
+
+#ifdef RDK_BUILD_COORDGEN_SUPPORT
+  bool oprefer = RDDepict::preferCoordGen;
+  RDDepict::preferCoordGen = useCoordGen;
+#endif
+  RDKit::ROMol *refPattern = nullptr;
+  int confId = -1;
+  RDKit::MatchVectType match = RDDepict::generateDepictionMatching2DStructure(
+      *d_mol, *(templateMol.d_mol), confId, refPattern, acceptFailure, false,
+      allowRGroups);
+  if (!match.empty()) {
+    rj::Document doc;
+    doc.SetObject();
+    MinimalLib::get_sss_json(*d_mol, *templateMol.d_mol, match, doc, doc);
+    rj::StringBuffer buffer;
+    rj::Writer<rj::StringBuffer> writer(buffer);
+    doc.Accept(writer);
+    res = buffer.GetString();
+  } else {
+    res = "{}";
+  }
+#ifdef RDK_BUILD_COORDGEN_SUPPORT
+  RDDepict::preferCoordGen = oprefer;
+#endif
+  return res;
+};
+
+JSSubstructLibrary::JSSubstructLibrary(unsigned int num_bits)
+    : d_sslib(new SubstructLibrary(
+          boost::shared_ptr<CachedTrustedSmilesMolHolder>(
+              new CachedTrustedSmilesMolHolder()),
+          boost::shared_ptr<PatternHolder>(new PatternHolder()))),
+      d_num_bits(num_bits) {
+  d_molHolder = dynamic_cast<CachedTrustedSmilesMolHolder *>(
+      d_sslib->getMolHolder().get());
+  d_fpHolder = dynamic_cast<PatternHolder *>(d_sslib->getFpHolder().get());
+}
+
+int JSSubstructLibrary::add_trusted_smiles(const std::string &smi) {
+  std::unique_ptr<RWMol> mol(SmilesToMol(smi, 0, false));
+  if (!mol) {
+    return -1;
+  }
+  mol->updatePropertyCache();
+  ExplicitBitVect *bv = PatternFingerprintMol(*mol, d_num_bits);
+  if (!bv) {
+    return -1;
+  }
+  d_fpHolder->addFingerprint(bv);
+  auto ret = d_molHolder->addSmiles(smi);
+  return ret;
+}
+
+inline int JSSubstructLibrary::add_mol_helper(const ROMol &mol) {
+  std::string smi = MolToSmiles(mol);
+  return add_trusted_smiles(smi);
+}
+
+int JSSubstructLibrary::add_mol(const JSMol &m) {
+  return add_mol_helper(*m.d_mol);
+}
+
+int JSSubstructLibrary::add_smiles(const std::string &smi) {
+  std::unique_ptr<RWMol> mol(SmilesToMol(smi));
+  if (!mol) {
+    return -1;
+  }
+  return add_mol_helper(*mol);
+}
+
+JSMol *JSSubstructLibrary::get_mol(unsigned int i) {
+  return new JSMol(new RWMol(*d_sslib->getMol(i)));
+}
+
+std::string JSSubstructLibrary::get_matches(const JSMol &q, bool useChirality,
+                                            int numThreads,
+                                            int maxResults) const {
+  if (!d_sslib->size()) {
+    return "[]";
+  }
+  std::vector<unsigned int> indices = d_sslib->getMatches(
+      *q.d_mol, true, useChirality, false, numThreads, maxResults);
+  rj::Document doc;
+  doc.SetArray();
+  auto &alloc = doc.GetAllocator();
+  for (const auto &i : indices) {
+    doc.PushBack(i, alloc);
+  }
+  rj::StringBuffer buffer;
+  rj::Writer<rj::StringBuffer> writer(buffer);
+  doc.Accept(writer);
+  std::string res = buffer.GetString();
+  return res;
+}
+
+unsigned int JSSubstructLibrary::count_matches(const JSMol &q,
+                                               bool useChirality,
+                                               int numThreads) const {
+  return d_sslib->countMatches(*q.d_mol, true, useChirality, false, 1);
+}
+
+std::string get_inchikey_for_inchi(const std::string &input) {
+  return InchiToInchiKey(input);
+}
+
+JSMol *get_mol(const std::string &input, const std::string &details_json) {
+  RWMol *mol = MinimalLib::mol_from_input(input, details_json);
+  return new JSMol(mol);
+}
+
+JSMol *get_qmol(const std::string &input) {
+  RWMol *mol = MinimalLib::qmol_from_input(input);
+  return new JSMol(mol);
+}
+
+std::string version() { return std::string(rdkitVersion); }
+
+void prefer_coordgen(bool useCoordGen) {
+#ifdef RDK_BUILD_COORDGEN_SUPPORT
+  RDDepict::preferCoordGen = useCoordGen;
+#endif
+}
